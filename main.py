fd5fa8eb4ad58d24975a5a6d39f3978a0dc68285
joaovgs <91490422+joaovgs@users.noreply.github.com>
2024-06-08 19:02:34 -0300

    Descrição das alterações feitas

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import engine, Base, get_db
-from models import Buyer, Item, Bid
-from schemas import BuyerCreate, ItemCreate, BidCreate, Buyer, Item, Bid
+from models import Comprador, Item, Lance
+from schemas import CompradorCreate, ItemCreate, LanceCreate, CompradorSchema, ItemSchema, LanceSchema
 import crud
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-@app.post("/buyers/", response_model=Buyer)
-def create_buyer(buyer: BuyerCreate, db: Session = Depends(get_db)):
-    return crud.create_buyer(db=db, buyer=buyer)
+@app.post("/compradores/", response_model=CompradorSchema)
+def criar_comprador(comprador: CompradorCreate, db: Session = Depends(get_db)):
+    return crud.criar_comprador(db=db, comprador=comprador)
 
-@app.post("/items/", response_model=Item)
-def create_item(item: ItemCreate, db: Session = Depends(get_db)):
-    return crud.create_item(db=db, item=item)
+@app.get("/compradores/", response_model=list[CompradorSchema])
+def listar_compradores(db: Session = Depends(get_db)):
+    return crud.pegar_compradores(db)
 
-@app.post("/bids/", response_model=Bid)
-def create_bid(bid: BidCreate, db: Session = Depends(get_db)):
+@app.post("/itens/", response_model=ItemSchema)
+def criar_item(item: ItemCreate, db: Session = Depends(get_db)):
+    return crud.criar_item(db=db, item=item)
+
+@app.get("/itens/", response_model=list[ItemSchema])
+def listar_itens(db: Session = Depends(get_db)):
+    return crud.pegar_itens(db)
+
+@app.post("/lances/", response_model=LanceSchema)
+def criar_lance(lance: LanceCreate, db: Session = Depends(get_db)):
     try:
-        return crud.create_bid(db=db, bid=bid)
+        return crud.criar_lance(db=db, lance=lance)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-@app.get("/items/", response_model=list[Item])
-def read_items(db: Session = Depends(get_db)):
-    items = crud.get_items(db)
-    return items
+@app.get("/lances/", response_model=list[LanceSchema])
+def listar_lances(db: Session = Depends(get_db)):
+    return crud.pegar_lances(db)
